9956e9c2c8d73c9ea04f2c5db6ae95521f15984b
dimitrovskif <filipdimitrovski22@gmail.com>
2015-03-01 14:23:38 +0100

    Made default mapfile cwd-independent

--- verushevskicypher.py
+++ verushevskicypher.py
@@ -1,6 +1,4 @@
-from sys import argv
 import json, argparse
-
 
 def get_encode_map(file_name):
     file = open(file_name, 'r')
@@ -12,23 +10,30 @@ def get_encode_map(file_name):
         raise IOError()
     
 def encode(message, encode_map):
-    return "_".join([str(encode_map[char]) for char in message])
+    return '_'.join([str(encode_map[char]) for char in message])
 
 def decode(message, encode_map):
     decode_map = {v: k for k, v in encode_map.items()}
-    return "".join([decode_map[int(code)] for code in message.split("_")])
+    return ''.join([decode_map[int(code)] for code in message.split('_')])
+
+def get_default_mapfile():
+    file_dir = os.path.dirname(os.path.realpath(__file__))
+    map_file = os.path.join(file_dir, 'encode_map.json')
+    return map_file
+    
+DEFAULT_MAPFILE = get_default_mapfile()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("mode", choices=['encode', 'decode'])
-    parser.add_argument("message", nargs='*')
-    parser.add_argument("--mapfile", default="encode_map.json")
+    parser.add_argument('mode", choices=['encode', 'decode'])
+    parser.add_argument('message", nargs='*')
+    parser.add_argument('--mapfile', default=DEFAULT_MAPFILE)
     
     arguments = parser.parse_args()
     
     map_file = arguments.mapfile
     encode_map = get_encode_map(map_file)
     if arguments.mode == 'encode':
-        print(encode(" ".join(arguments.message), encode_map))
+        print(encode(' '.join(arguments.message), encode_map))
     else:
-        print(decode(" ".join(arguments.message), encode_map))
+        print(decode(' '.join(arguments.message), encode_map))
